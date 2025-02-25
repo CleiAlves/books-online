a0b24c801bf3356a95563816a0d9c7f1f4bc101b
Cleison Alves <cleison_nzo@hotmail.com>
2025-02-25 15:13:30 -0300

    Add campo para foto dos autores

--- authors/models.py
+++ authors/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='authors/', blank=True, null=True)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
