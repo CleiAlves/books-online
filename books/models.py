2929203f2fb09df29caf33fc07da987b8a289d9d
Cleison Alves <cleison_nzo@hotmail.com>
2025-03-12 15:53:22 -0300

    Salvando imagens reduzidas para os livros

--- books/models.py
+++ books/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from authors.models import Author
+from PIL import Image
+import os
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
@@ -8,6 +10,20 @@ class Book(models.Model):
     genre = models.CharField(max_length=50)
     cover_image = models.ImageField(upload_to='book_covers/', blank=True, null=True)
 
+
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs) # salva a imagem original
+
+        if self.cover_image:
+            img_path = self.cover_image.path
+            img = Image.open(img_path)
+
+            # Define o tamanho máximo para a miniatura
+            max_size = (200, 250)  
+            img.thumbnail(max_size)  # Redimensiona mantendo a proporção
+            
+            img.save(img_path, format="JPEG", quality=80)  # Salva a imagem otimizada
 
     def __str__(self):
         return self.title
